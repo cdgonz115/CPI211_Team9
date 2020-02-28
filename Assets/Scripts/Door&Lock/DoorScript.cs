3015d633850b7ac3ccfce1777562c5e0ad0d56ff
CloudWannabe <55220508+CloudWannabe@users.noreply.github.com>
2020-02-28 12:24:58 -0700

    Doors&Lock
    DoorScript - for doors. checked if player if close enough and allows them to open door if keycount is > 0
    KeyScript - for keys. checked if player if close enough and allows them to pick up keys
    Inventory- might remove, supposed to just hold keycount

---
+++ Assets/Scripts/Door&Lock/DoorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Animation DoorOpen;
+    private bool doorArea = false;
+
+    //public static int keyCount; maybe put here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && doorArea == true && keyCount > 0)
+        {
+            GameObject.Find("Door").GetComponent<Animation>.Play("DoorOpen");
+            keyCount--;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "player")
+        {
+            doorArea = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "player")
+        {
+            doorArea = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Door&Lock/KeyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+
+    private bool keyArea = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory.keyCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && keyArea == true)
+        {
+            Inventory.keyCount++;
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            keyArea = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            keyArea = false;
+        }
+    }
+}
